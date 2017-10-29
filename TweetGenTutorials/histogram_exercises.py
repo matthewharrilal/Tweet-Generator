7c24b65e6aeb934b5733b0c1b13a8e8b0f513ac6
matthewharrilal <matthewharrilal@gmail.com>
2017-10-29 16:38:21 -0700

    Finished the frequency function

--- TweetGenTutorials/histogram_exercises.py
+++ TweetGenTutorials/histogram_exercises.py
@@ -6,16 +6,17 @@ read_file = word_file.read()
 split_file = read_file.split()
 word_list = ["Matthew", "Corey", "Harrilal", "Corey", "Justin", "Justin", "Steven"]
 
-def find_frequency_of_words(split_file):
+users_input = str(input())
+# users_input = "Corey"
+def find_frequency_of_words(users_input, word_list):
    word_frequency = {}
    # So essentially we want a key value pair where the key represents the unique wor
-   for word in split_file:
-       occurences = split_file.count(word)
-       word_frequency[word] = occurences
-       # if occurences == 1:
-       #     word_frequency[word] = occurences
+   if users_input in word_list:
+       for _ in word_list:
+           occurences = word_list.count(users_input)
+           word_frequency[users_input] = occurences
    return word_frequency
 
 
 
-print(find_frequency_of_words(word_list))
+print(find_frequency_of_words(users_input, word_list))
