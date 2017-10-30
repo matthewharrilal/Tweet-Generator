957090f8833b37b03ed1b55e9e794c2a15fd6990
matthewharrilal <matthewharrilal@gmail.com>
2017-10-29 18:09:54 -0700

    Finished the histogram function

--- TweetGenTutorials/histogram_exercises.py
+++ TweetGenTutorials/histogram_exercises.py
@@ -4,7 +4,7 @@ from functools import reduce
 word_file = open("/usr/share/dict/words")
 read_file = word_file.read()
 split_file = read_file.split()
-word_list = ["Matthew", "Corey", "Harrilal", "Corey", "Justin", "Justin", "Steven", "Rohan", "Willie"]
+word_list = ["Matthew", "Corey", "Harrilal", "Corey", "Justin", "Justin", "Steven", "Rohan", "Willie", "Willie", "Elliot"]
 
 # users_input = str(input())
 # users_input = "Corey"
@@ -28,5 +28,14 @@ def find_unique_words(word_list):
 
 
 
-print(find_unique_words(word_list))
+def histogram(word_list):
 
+# So essentially what this function will do is that it will take the unique words as a key in a dictionary and as a value the freu
+   word_frequency = {}
+   for word in word_list:
+       occurences = word_list.count(word)
+       word_frequency[word] = occurences
+   return word_frequency
+
+
+print(histogram(word_list))
