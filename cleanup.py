068841f1d8bd6fdf04f81d8af2a0b76a47291227
matthewharrilal <matthewharrilal@gmail.com>
2017-11-08 08:18:19 -0800

    Reformatted the class

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,6 @@
 
 
+
 def clean_given_text(word_text):
     word_file = open(word_text)
     read_file = word_file.read()
---
+++ sample.py
@@ -0,0 +1,2 @@
+class Sample_Words_From_Histogram(str):
+    '''Samples a word from the histogram using the weights of the word'''
