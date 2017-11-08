2eac5144460a2371593a862b14aa38e5a0fcf2c8
matthewharrilal <matthewharrilal@gmail.com>
2017-11-08 08:09:40 -0800

    Generates a histogram using a class as well as implemented the cleanup file

--- ClassDictogram.py
+++ ClassDictogram.py
@@ -1,1 +1,23 @@
-import histogram_exercises
+
+import cleanup
+import pdb
+
+class Dictogram(dict):
+    # def __init__(self, word_text):
+    #     '''Everytime this dictogram class is instantiated word text is given'''
+    #     word_text = word_text
+    #
+
+'''Generates a dictogram given a piece of text'''
+    def generate_histogram(self, word_text):
+        '''This function generates our histogram for us'''
+        word_frequency = {}
+        cleaned_text = cleanup.clean_given_text("robert_greene.txt")[:1000]
+        for word in cleaned_text:
+            word_occurences = cleaned_text.count(word)
+            word_frequency[word] = word_occurences
+        return word_frequency
+
+
+dictogram = Dictogram()
+print(dictogram.generate_histogram("robert_greene.txt"))
---
+++ cleanup.py
@@ -0,0 +1,8 @@
+
+
+def clean_given_text(word_text):
+    word_file = open(word_text)
+    read_file = word_file.read()
+    split_file = read_file.split()
+    return split_file
+
