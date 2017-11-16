3569f528865bb9babd28de4c728292eb31c6ec9d
matthewharrilal <matthewharrilal@gmail.com>
2017-11-15 17:38:24 -0800

    Making progress on the functions

--- linkedlist.py
+++ linkedlist.py
@@ -18,7 +18,7 @@ class LinkedList(object):
 
     def __init__(self, items=None):
         """Initialize this linked list and append the given items, if any."""
-        self.head = None  # First node
+        self.head = Node(items)  # First node we have to set the head to current node because the head will always have to have a pointer to the first node the first node is none it is a pointer
         self.tail = None  # Last node
         # Append given items
         if items is not None:
@@ -58,11 +58,45 @@ class LinkedList(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all nodes and count one for each
 
+        # First thing we essentially need is the first node
+        current_node = self.head
+
+        # Then we need a counter to see how many times thee while loop iterates over the list
+        total = 0
+
+        # This is similar to essentially stopping a list index out of range since the current node is always looking to the next node if the loop is done iterating
+        # it will stop at the last node and make that the current node, but that node doesn't exist therefore it will crash however if you set the last node to the next node
+        # we can stop the loop and outside of the loop to simpley make that the current node however we only care about making that the current node if we have to use the current node
+        # here we only care about the count and even thought the iteration stops on the next node we still iterate over it fully before crash
+        while current_node.next != None:
+
+            # Essentially in these lines of code what we are doing is that we are incrementing the count everytime we iterate and then from there we set the current node to the next node
+            # so that the list doesnt crash when the loop is over
+            total += total
+            current_node = current_node.next
+        return total
+
+
     def append(self, item):
         """Insert the given item at the tail of this linked list.
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Create new node to hold given item
         # TODO: Append node after tail, if it exists
+
+        # First we are creating a new node this node does not know that it is the newest node just yet just know it is the current node
+        new_node = Node(item)
+
+        # We are setting the head to the current node
+        current_node = self.head
+
+        # We know that when the current nodes next node is none we have reached the end of our linked list therefore we stop the loop
+        while current_node.next != None:
+            # Everytime we iterate we succesfully have a new node therefore we want to set that new node to the current node so the next iteration can begin
+            current_node = current_node.next
+
+        # And then when this iteration is done we should be left of with the next node because the loop stops before the last node can become the current node
+        # therefore we then want to set that last node to the current node therefore we can know that the current node now points to nothing therefore the end of the list
+        current_node.next = new_node
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
