e3c4735189fd8d6913ff3efdb9f9a705c0f80f8c
matthewharrilal <matthewharrilal@gmail.com>
2017-11-29 23:37:14 -0800

    Making progress on the hash functions

--- hashtable.py
+++ hashtable.py
@@ -21,4 +21,33 @@ class HashTable:
 
     def add(self, key, value):
         # Gets the index of the key that the user passes in
-        key_hash = self.get_index(key)
+        key_index = self.get_index(key)
+
+        # Since we are using lists we are constructing a list that is a key value pair
+        key_value = [key, value]
+
+        # First we have to see if what the user is trying to add even exists and if it doesnt then at that index
+        # that we have gotten from the key index variable that we update the value at that index with that list comprised
+        # of the key value pair
+        if self.map[key_index] is None:
+            # This is checking if there any values even in that bucket and the way that it is doing that is oddly enough
+            # even though the user is trying to find the specific key when we get and come across that index we are
+            # presented with all the values instead so this is checking if there are even any values in that index and if
+            # there is not you wouldn't be appending to anything you just construct a new list
+            self.map[key_index] = list([key_value])
+            return 'The index at the key was non existent therefore a new list was added comprised of the key value pair'
+
+        # Now we are checking if the value does exist at that index we are going to want to update the value at that key
+        else:
+            # We are accessing the list at that index the key value pair in that list
+            for pair in self.map[key_index]:
+                # Now that we are checking if the key does actually match what we are looking for but you are probably
+                # wondering dont we know that already but in the case that there was a collision we can have mulitple
+                # lists in once bucket therefore this confirms if we are in the right list in the bucket
+                if pair[0] == key:
+
+                    # Then update the value
+                    pair[1] = value
+                    return True
+            # And if the key that the user is trying to append is a key that we can not find in the list we then append the key value pair
+            self.map[key_index].append(key_value)
